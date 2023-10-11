7f991e4e415101fc5ca226edf3bff2b303df3fa1
Hanne-Hellin-MCT <hanne.hellin@student.howest.be>
2023-10-11 10:33:01 +0200

    eind

--- script/app.js
+++ script/app.js
@@ -1,5 +1,6 @@
 const jokeContent = document.querySelector('.js-joke-content');
 const jokeButton = document.querySelector('.js-joke-button');
+const jokeLoader = document.querySelector('.js-loading-state');
 
 const fetchJoke = async () => {
     return fetch('https://icanhazdadjoke.com/', {
@@ -11,18 +12,36 @@ const fetchJoke = async () => {
         .then((data) => data.joke); // Extract the 'joke' property from the response JSON
 };
 
-const generateJoke = async () => {
+const showLoader = () => {
+    loadingDelayed = setTimeout(() => {
+        jokeLoader.classList.remove('u-hidden');
+    }, 300)
 
+    jokeContent.classList.add('u-hidden');
+};
+
+const removeLoader = () => {
+    if (loadingDelayed) {
+        clearTimeout(loadingDelayed)
+        loadingDelayed = null
+    }
+    jokeLoader.classList.add('u-hidden'); // Fixed typo: classlist should be classList
+    jokeContent.classList.remove('u-hidden');
+};
+
+const generateJoke = async () => {
+    showLoader();
     const joke = await fetchJoke();
+
     console.log(joke);
     jokeContent.innerHTML = joke;
-
+    removeLoader();
 };
 
 const init = function () {
     console.log('script loaded');
     generateJoke();
-    jokeButton.addEventListener('click', generateJoke)
+    jokeButton.addEventListener('click', generateJoke);
 };
 
 init();
