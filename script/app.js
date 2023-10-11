dbf1b1038a7db732c75e714f2306984886a9344f
Hanne-Hellin-MCT <hanne.hellin@student.howest.be>
2023-10-11 09:36:02 +0200

    designen + basic werking

--- script/app.js
+++ script/app.js
@@ -1,5 +1,28 @@
+const jokeContent = document.querySelector('.js-joke-content');
+const jokeButton = document.querySelector('.js-joke-button');
+
+const fetchJoke = async () => {
+    return fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            Accept: 'application/json',
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => data.joke); // Extract the 'joke' property from the response JSON
+};
+
+const generateJoke = async () => {
+
+    const joke = await fetchJoke();
+    console.log(joke);
+    jokeContent.innerHTML = joke;
+
+};
+
 const init = function () {
     console.log('script loaded');
-}
+    generateJoke();
+    jokeButton.addEventListener('click', generateJoke)
+};
 
 init();
